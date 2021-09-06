eb4f3d17fd928b719a87c4a2d3943ac9692bdacf
Arias <progweb@orbitadigital.com>
2021-09-06 11:39:37 +0200

    Correct some mistakes

--- src/Resources.php
+++ src/Resources.php
@@ -6,7 +6,6 @@ namespace OrbitaDigital\OdFirst;
 
 use Db;
 
-// TODO esto que es y para que lo usas?
 class Resources
 {
     /**
@@ -20,9 +19,9 @@ class Resources
     public function validate(string $nombre, string $edad, string $date)
     {
         $resultados = ["correcto" => [], "error" => []];
-        $cadena = array("nombre" => $nombre, "edad" => $edad, "date" => $date);
+        $cadena = ["nombre" => $nombre, "edad" => $edad, "date" => $date];
         foreach ($cadena as $key => $i) {
-            if (empty(trim($cadena[$key]))) {
+            if (empty(trim($i))) {
                 $resultados["error"][] = $key;
             } else {
                 $resultados["correcto"][] = $key;
@@ -53,14 +52,12 @@ class Resources
      * 
      * @return bool
      */
-    public function delete(string $nombre)
+    public function delete(int $id)
     {
-        $array = [];
-        if (count(Db::getInstance()->executeS("SELECT id FROM " . _DB_PREFIX_ . "od_first_formulario WHERE id='$nombre'"))) {
-            return Db::getInstance()->execute("UPDATE " . _DB_PREFIX_ . "od_first_formulario SET fecha_modificacion=NOW() , borrado=1 , fecha_borrado=NOW() WHERE id='$nombre'");
-        } else {
-            return $array;
+        if ($id > 0) {
+            return Db::getInstance()->execute("UPDATE " . _DB_PREFIX_ . "od_first_formulario SET borrado=1 , fecha_borrado=NOW() WHERE id='$id'");
         }
+        return [];
     }
     /**
      * Funcion mostrar tabla
@@ -75,16 +72,13 @@ class Resources
      */
     public function search(string $nombre, string $fecha_desde, string $fecha_hasta, string $borrado, string $fecha_eleccion, string $boton1)
     {
-        $array = array('Nombre' => $nombre, 'Fecha_creacion' => $fecha_desde, 'Fecha_creacion2' => $fecha_hasta, 'deleted' => $borrado);
+        $array = ['Nombre' => $nombre, 'Fecha_creacion' => $fecha_desde, 'Fecha_creacion2' => $fecha_hasta, 'deleted' => $borrado];
         $query = "SELECT * FROM "  . _DB_PREFIX_ .  "formulario WHERE ";
-        $query2 = "SELECT * FROM " . _DB_PREFIX_ . "formulario WHERE ";
         $where = [];
         $rows = 5;
-        // echo $fecha_eleccion;
         $page = intval($boton1);
         $limit = ($page * $rows);
         $offset = $rows;
-        // echo $fecha_eleccion;
         foreach ($array as $columna => $valor) {
             if ($columna == 'Nombre') {
                 if (!empty(trim($valor))) {
@@ -109,16 +103,15 @@ class Resources
             }
             continue;
         }
-        $query2 .= implode(' AND ', $where);
-        $numero_registros = count(Db::getInstance()->executeS($query2));
-        // echo $query2;
+
         $query .= implode(' AND ', $where);
+        if (Db::getInstance()->executeS($query)) {
+            $numero_registros = count(Db::getInstance()->executeS($query));
+        }
         $query .= ' LIMIT ' . $limit . ',' . $offset;
-        // echo $query;
         return ["datos" => Db::getInstance()->executeS($query), "contador" => $numero_registros];
     }
 
-    // TODO no tiene explicacion
     /**
      * Funcion actualizar campos
      * @param string $nombre
@@ -130,31 +123,17 @@ class Resources
      */
     public function update(int $id)
     {
-        $array = array("id" => $id);
-        foreach ($array as $variables) {
-            if ($variables == 0) {
-                return $array;
-            }
+        if ($id > 0) {
+            return Db::getInstance()->execute("UPDATE " . _DB_PREFIX_ . "od_first_formulario SET borrado='0', fecha_borrado= NULL WHERE id= $id");
         }
-        return Db::getInstance()->execute("UPDATE " . _DB_PREFIX_ . "od_first_formulario SET borrado='0', fecha_borrado= NULL WHERE id= $id");
+        return [];
     }
-    public function update2(int $id)
-    {
-        $array = array("id" => $id);
-        foreach ($array as $variables) {
-            if ($variables == 0) {
-                return $array;
-            }
-        }
-        return Db::getInstance()->execute("UPDATE " . _DB_PREFIX_ . "od_first_formulario SET borrado='1', fecha_borrado= NOW() WHERE id= $id");
-    }
-
     /**
      * Funcion instalar tabla
      * 
      * @return bool
      */
-    public function installTable()
+    public static function installTable()
     {
         return Db::getInstance()->execute('CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'od_first_formulario`(`id` INT unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY, `nombre` VARCHAR(25) NOT NULL, `edad` INT NOT NULL, `fecha` Date NOT NULL, `fecha_creacion` datetime NOT NULL,`fecha_modificacion` datetime NOT NULL,`borrado` bool NOT NULL,`fecha_borrado` datetime NULL) ENGINE = ' . _MYSQL_ENGINE_ . ' CHARACTER SET utf8 COLLATE utf8_general_ci;');
     }
@@ -163,7 +142,7 @@ class Resources
      * 
      * @return bool
      */
-    public function uninstallTable()
+    public static function uninstallTable()
     {
         return Db::getInstance()->execute('DROP TABLE IF EXISTS `' . _DB_PREFIX_ . 'od_first_formulario`');;
     }
