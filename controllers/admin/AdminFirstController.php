6f5ee9c1d6b0652acd53565c27d3c3c92ccbcb02
Arias <progweb@orbitadigital.com>
2021-09-06 11:35:47 +0200

    Improve function getQuery and Correct one mistake on date filter from helperlist
    There was a mistake on date filter. Only can filter by two date. Now it can filter by one date

--- controllers/admin/AdminFirstController.php
+++ controllers/admin/AdminFirstController.php
@@ -121,24 +121,24 @@ class AdminFirstController extends ModuleAdminController
     {
         $where = [];
         foreach ($_POST as $key => $i) {
-            if (strstr($key, 'od_first_formularioFilter')  && !strstr($key, 'local_od_first_formularioFilter')) {
-                $palabra = substr($key, 26, strlen($key) - 1);
-                if ($palabra != 'fecha' &&  $palabra != 'fecha_creacion' && $palabra != 'fecha_modificacion' && $palabra != 'fecha_borrado') {
-                    if (isset($i)) {
-                        if ($palabra == 'nombre') {
-                            $where[] =  "$palabra LIKE '$i%'";
-                        } else {
-                            if (!empty($i)) {
-                                $where[] = "$palabra = '$i'";
-                            }
-                        }
-                    }
-                }
-            }
-            if (strstr($key, 'local_od_first_formularioFilter')) {
-                $palabra = substr($key, 32, strlen($key) - 1);
+            if (strpos($key, 'local') === 0) {
+                $palabra = str_replace('local_od_first_formularioFilter_', '', $key);
+                echo $palabra;
                 foreach ($this->dataQuery($palabra, $i) as $key => $i) {
                     $where[] = "$i";
+                }
+                continue;
+            }
+            if (strpos($key, 'od_first_formularioFilter') === 0) {
+                $palabra = str_replace('od_first_formularioFilter_', '', $key);
+                echo $palabra;
+                if (strpos($palabra, 'fecha') || empty(trim($i))) {
+                    continue;
+                }
+                if ($palabra == 'nombre') {
+                    $where[] =  "$palabra LIKE '$i%'";
+                } else {
+                    $where[] = "$palabra = '$i'";
                 }
             }
         }
@@ -146,13 +146,16 @@ class AdminFirstController extends ModuleAdminController
         if (count($where) > 0) {
             $result .= 'WHERE ';
         }
-        if (Tools::getValue('od_first_formularioOrderby')) {
-            $field = Tools::getValue('od_first_formularioOrderby');
-            $order = Tools::getValue('od_first_formularioOrderway');
-            $sentence = " ORDER BY $field $order";
+        $orderby = Tools::getValue('od_first_formularioOrderby');
+        $orderway = Tools::getValue('od_first_formularioOrderway');
+        if ($orderby) {
+            $sentence = " ORDER BY $orderby $orderway";
         }
         $result .= implode(' AND ', $where) . $sentence;
-        dump($where);
+        $_POST['od_first_formularioFilter_fecha'] = $_POST['local_od_first_formularioFilter_fecha'];
+        $_POST['od_first_formularioFilter_fecha_creacion'] = $_POST['local_od_first_formularioFilter_fecha_creacion'];
+        $_POST['od_first_formularioFilter_fecha_modificacion'] = $_POST['local_od_first_formularioFilter_fecha_modificacion'];
+        $_POST['od_first_formularioFilter_fecha_borrado'] = $_POST['local_od_first_formularioFilter_fecha_borrado'];
         return $result;
     }
 
@@ -170,10 +173,10 @@ class AdminFirstController extends ModuleAdminController
         $list->simple_header = false;
         $list->shopLinkType = '';
         $list->no_link = true;
-        $list->orderBy = Tools::getValue('od_first_formularioOrderby');
-        $list->orderWay = Tools::getValue('od_first_formularioOrderway');
-        $list->_pagination = array(1, 5, 10, 100, 200);
-        $list->_default_pagination = 100;
+        $list->orderBy = Tools::getValue('od_first_formularioOrderby', 'id');
+        $list->orderWay = Tools::getValue('od_first_formularioOrderway', 'asc');
+        $list->_pagination = [1, 5, 10, 20, 50];
+        $list->_default_pagination = 20;
         $list->listTotal = count($total);
         $list->token = Tools::getAdminTokenLite('AdminFirst');
         $list->identifier = 'id';
@@ -181,38 +184,38 @@ class AdminFirstController extends ModuleAdminController
         $list->currentIndex = AdminController::$currentIndex;
 
 
-        $fields_list = array(
+        $fields_list = [
             'id' => [
                 'title' => $this->l('ID'),
                 'type' => 'text',
                 'search' => false,
             ],
-            'nombre' => array(
+            'nombre' => [
                 'title' => $this->l('Nombre'),
                 'width' => 140,
                 'type' => 'text',
-            ),
-            'edad' => array(
+            ],
+            'edad' => [
                 'title' => $this->l('Edad'),
                 'width' => 140,
                 'type' => 'number',
-            ),
-            'fecha' => array(
+            ],
+            'fecha' => [
                 'title' => $this->l('Fecha'),
                 'width' => 140,
                 'type' => 'date',
-            ),
-            'fecha_creacion' => array(
+            ],
+            'fecha_creacion' => [
                 'title' => $this->l('Fecha de creacion'),
                 'width' => 140,
                 'type' => 'datetime',
-            ),
-            'fecha_modificacion' => array(
+            ],
+            'fecha_modificacion' => [
                 'title' => $this->l('Fecha de modificacion'),
                 'width' => 140,
                 'type' => 'datetime',
-            ),
-            'borrado' => array(
+            ],
+            'borrado' => [
                 'title' => $this->l('Borrado'),
                 'width' => 140,
                 'type' => 'number',
@@ -221,30 +224,22 @@ class AdminFirstController extends ModuleAdminController
                     0 => 'enabled.gif',
                     1 => 'disabled.gif',
                 ],
-            ),
-            'fecha_borrado' => array(
+            ],
+            'fecha_borrado' => [
                 'title' => $this->l('Fecha de borrado'),
                 'width' => 140,
                 'type' => 'datetime',
-            ),
-        );
+            ],
+        ];
         if (Tools::isSubmit('submitResetod_first_formulario')) {
             AdminController::processResetFilters($fields_list);
             $total = Db::getInstance()->executeS($this->getQuery());
             $resultado = Db::getInstance()->executeS($this->getQuery() . $this->pagination());
         }
-        if (Tools::isSubmit('submitFilterod_first_formulario')) {
-            $fecha[] = Tools::getValue('local_od_first_formularioFilter_fecha');
-            $fecha_creaccion[] = Tools::getValue('local_od_first_formularioFilter_fecha_creacion');
-            $fecha_modificacion[] = Tools::getValue('local_od_first_formularioFilter_fecha_modificacion');
-            $fecha_borrado[] = Tools::getValue('local_od_first_formularioFilter_fecha_borrado');
-        }
         return $list->generateList($resultado, $fields_list);
     }
     public function initContent()
     {
-        dump($_POST);
-        dump($_GET);
         $check = 0;
         if (Tools::isSubmit('submitFilterod_first_formulario') || Tools::getValue('od_first_formularioOrderby')) {
             $check = 1;
